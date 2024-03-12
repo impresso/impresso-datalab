c1de209310474e477158bbf4bc7242801eaca93e
Daniele Guido <gui.daniele@gmail.com>
2024-03-12 17:28:01 +0100

    Update Tutorial.js

--- src/templates/Tutorial.js
+++ src/templates/Tutorial.js
@@ -3,13 +3,25 @@ import { Col, Container, Row } from "react-bootstrap"
 import Markdown from "../components/Markdown"
 
 const Tutorial = ({ path, pageContext, ...props }) => {
+  const modules = pageContext.data.body.split("\n```")
   return (
     <div className="Tutorial">
       <Container>
         <h1>{pageContext.data.title}</h1>
         <Row>
           <Col>
-            <Markdown>{pageContext.data.body}</Markdown>
+            {modules.map((module, i) => {
+              const isCodeBlock = i % 2 === 1
+              if (isCodeBlock) {
+                return (
+                  <pre key={i} className="bg-dark text-light p-3">
+                    {module}
+                  </pre>
+                )
+              } else {
+                return <Markdown key={i}>{module}</Markdown>
+              }
+            })}
           </Col>
           <Col>
             {pageContext.data.excerpt}
@@ -21,8 +33,6 @@ const Tutorial = ({ path, pageContext, ...props }) => {
           </Col>
         </Row>
       </Container>
-      {JSON.stringify(path)}
-      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   )
 }
