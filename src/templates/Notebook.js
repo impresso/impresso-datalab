a1b04c4a291b33d0b8d81a35a65846bad75b0c10
donsiamese <donsiamese@gmail.com>
2024-05-13 14:49:55 +0200

    html structure update

--- src/templates/Notebook.js
+++ src/templates/Notebook.js
@@ -2,16 +2,28 @@ import React, { useRef } from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import Markdown from "../components/Markdown"
 import CodeSnippet from "../components/CodeSnippet"
+import { DateTime } from "luxon"
+import { Calendar } from "iconoir-react"
 
 const Notebook = ({ path, pageContext, ...props }) => {
+  const accessTime = pageContext?.data.accessTime
+  const accessDateTime = DateTime.fromISO(accessTime)
   const codeMirrorRef = useRef(null)
 
   return (
     <div className="Notebook">
       <Container>
-        <h1>{pageContext.data.title}</h1>
+        <h1 className="mb-4">{pageContext.data.title}</h1>
+
         <Row>
           <Col lg="8">
+            <div className="date">
+              <i className="me-1">
+                <Calendar width={18} />
+              </i>
+              <span>{accessDateTime.toFormat("yyyy LLL dd")}</span>
+            </div>
+            <hr />
             <Markdown>{pageContext.data.body}</Markdown>
             {JSON.stringify(path)}
 
--- src/templates/Tutorial.js
+++ src/templates/Tutorial.js
@@ -1,35 +1,48 @@
 import React, { useRef } from "react"
 import { Col, Container, Row, Button } from "react-bootstrap"
 import Markdown from "../components/Markdown"
-import { GitFork, ArrowDown } from "iconoir-react"
+import { GitFork, ArrowDown, Calendar } from "iconoir-react"
 import LogoColab from "../components/_svg/LogoColab"
 import CodeSnippet from "../components/CodeSnippet"
+import { DateTime } from "luxon"
+import Badge from "../components/Badge"
 
 const Tutorial = ({ path, pageContext, ...props }) => {
   const modules = pageContext.data.body.split("\n```")
+  const accessTime = pageContext?.data.accessTime
+  const accessDateTime = DateTime.fromISO(accessTime)
 
   const codeMirrorRef = useRef(null)
 
   return (
     <div className="Tutorial">
       <Container>
-        <h1>{pageContext.data.title}</h1>
-        <div className="button-group d-flex my-3">
-          <Button className="me-2" variant="primary">
-            <LogoColab width={20} className="me-1" />
-            <span>Open in colab</span>
-          </Button>
-          <Button className="me-2" variant="secondary">
-            <GitFork className="me-1" strokeWidth={2} />
-            <span>Fork...</span>
-          </Button>
-          <Button variant="secondary">
-            <ArrowDown className="me-1" strokeWidth={2} />
-            <span>Download</span>
-          </Button>
-        </div>
+        <h1 className="mb-4">{pageContext.data.title}</h1>
+        <Badge value="P2c" color="var(--secondary)" />
+
         <Row>
           <Col>
+            <div className="date">
+              <i className="me-1">
+                <Calendar width={18} />
+              </i>
+              <span>{accessDateTime.toFormat("yyyy LLL dd")}</span>
+            </div>
+            <div className="button-group d-flex my-3">
+              <Button className="me-2" variant="primary">
+                <LogoColab width={20} className="me-1" />
+                <span>Open in colab</span>
+              </Button>
+              <Button className="me-2" variant="secondary">
+                <GitFork className="me-1" strokeWidth={2} />
+                <span>Fork...</span>
+              </Button>
+              <Button variant="secondary">
+                <ArrowDown className="me-1" strokeWidth={2} />
+                <span>Download</span>
+              </Button>
+            </div>
+            <hr />
             {modules.map((module, i) => {
               const isCodeBlock = i % 2 === 1
               if (isCodeBlock) {
