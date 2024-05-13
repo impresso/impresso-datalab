ef3ef59f6c2dcff31611a345c75d92132662fa09
donsiamese <donsiamese@gmail.com>
2024-05-13 14:48:16 +0200

    add copied state

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -1,18 +1,26 @@
 import React from "react"
+import { useState } from "react"
 import ReactCodeMirror from "@uiw/react-codemirror"
 import { nord } from "@uiw/codemirror-theme-nord"
 import { python } from "@codemirror/lang-python"
-import { Copy } from "iconoir-react"
+import { Copy, CheckCircle } from "iconoir-react"
 import { Button } from "react-bootstrap"
 import "./CodeSnippet.css"
 
-const CodeSnippet = ({ codeMirrorRef, value, key }) => {
+const CodeSnippet = ({ codeMirrorRef, value, key = 0 }) => {
+  const [icon, setIcon] = useState("original")
+
   const handleCopy = () => {
     if (codeMirrorRef.current) {
+      setIcon("new")
+
+      // Reset the icon after a few seconds
+      setTimeout(() => {
+        setIcon("original")
+      }, 2000) // 2000 milliseconds = 2 seconds
       // Access the value directly from the CodeMirror component
       const code = codeMirrorRef.current.state.doc
       // Copy the code to the clipboard
-      console.log("code", code)
       navigator.clipboard
         .writeText(code)
         .then(() => {
@@ -23,6 +31,7 @@ const CodeSnippet = ({ codeMirrorRef, value, key }) => {
         })
     }
   }
+  console.log("info", key)
   return (
     <div className="code-wrapper" key={key}>
       <ReactCodeMirror
@@ -32,7 +41,11 @@ const CodeSnippet = ({ codeMirrorRef, value, key }) => {
         extensions={[python()]}
       />
       <button className="copy-code" onClick={handleCopy} aria-label="copy code">
-        <Copy strokeWidth={2} />
+        {icon === "original" ? (
+          <Copy strokeWidth={2} />
+        ) : (
+          <CheckCircle strokeWidth={2} />
+        )}
       </button>
       <Button className="me-2" variant="primary" size="sm">
         {/* <LogoColab width={20} className="me-1" /> */}
