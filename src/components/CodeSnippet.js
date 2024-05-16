7a03d9f5a3099c37934db0f80eb4a5164e9c966d
donsiamese <donsiamese@gmail.com>
2024-05-16 13:39:48 +0200

    CodeSnippet - copy button update

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { useState } from "react"
+import React, { useEffect } from "react"
+import { useState, useRef } from "react"
 import ReactCodeMirror from "@uiw/react-codemirror"
 import { nord } from "@uiw/codemirror-theme-nord"
 import { python } from "@codemirror/lang-python"
@@ -7,16 +7,19 @@ import { Copy, CheckCircle } from "iconoir-react"
 import { Button } from "react-bootstrap"
 import "./CodeSnippet.css"
 
-const CodeSnippet = ({ codeMirrorRef, value, key = 0 }) => {
-  const [icon, setIcon] = useState("original")
+const CodeSnippet = ({ value, key = 0 }) => {
+  const t = useRef()
+  const codeMirrorRef = useRef(null)
+  const [isActive, setIsActive] = useState(false)
 
   const handleCopy = () => {
     if (codeMirrorRef.current) {
-      setIcon("new")
+      clearTimeout(t.current)
+      setIsActive(true)
 
       // Reset the icon after a few seconds
-      setTimeout(() => {
-        setIcon("original")
+      t.current = setTimeout(() => {
+        setIsActive(false)
       }, 2000) // 2000 milliseconds = 2 seconds
       // Access the value directly from the CodeMirror component
       const code = codeMirrorRef.current.state.doc
@@ -31,9 +34,12 @@ const CodeSnippet = ({ codeMirrorRef, value, key = 0 }) => {
         })
     }
   }
+
+  useEffect(() => clearTimeout(t.current), [])
+
   console.log("info", key)
   return (
-    <div className="code-wrapper" key={key}>
+    <div className="CodeSnippet" key={key}>
       <ReactCodeMirror
         ref={codeMirrorRef}
         value={value}
@@ -41,11 +47,7 @@ const CodeSnippet = ({ codeMirrorRef, value, key = 0 }) => {
         extensions={[python()]}
       />
       <button className="copy-code" onClick={handleCopy} aria-label="copy code">
-        {icon === "original" ? (
-          <Copy strokeWidth={2} />
-        ) : (
-          <CheckCircle strokeWidth={2} />
-        )}
+        {isActive ? <CheckCircle strokeWidth={2} /> : <Copy strokeWidth={2} />}
       </button>
       <Button className="me-2" variant="primary" size="sm">
         {/* <LogoColab width={20} className="me-1" /> */}
