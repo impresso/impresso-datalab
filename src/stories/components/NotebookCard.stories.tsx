75cdc7992ce476ba1ebe1a57879ca094b2ce5a16
Daniele Guido <gui.daniele@gmail.com>
2024-09-17 10:36:45 +0200

    init storybook with three components

---
+++ src/stories/components/NotebookCard.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react"
+// import { fn } from "@storybook/test"
+import NotebookCard from "../../components/NotebookCard"
+import type { Notebook } from "../../components/NotebookCard"
+
+const meta: Meta<typeof NotebookCard> = {
+  component: NotebookCard,
+}
+
+export default meta
+type Story = StoryObj<typeof NotebookCard>
+
+export const Default: Story = {
+  args: {
+    notebook: {
+      slug: "notebook-1",
+      href: "/notebook-1",
+      title: "Notebook 1",
+      authors: [
+        {
+          name: "Author 1",
+        },
+      ],
+      date: new Date(),
+    } as Notebook,
+  },
+}
---
+++ src/stories/components/Token.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react"
+// import { fn } from "@storybook/test"
+import Token from "../../components/Token"
+import type { TokenProps } from "../../components/Token"
+
+const meta: Meta<typeof Token> = {
+  component: Token,
+  render: (args) => (
+    <div style={{ maxWidth: "400px" }}>
+      <Token {...args} />
+    </div>
+  ),
+}
+
+export default meta
+type Story = StoryObj<typeof Token>
+
+export const Default: Story = {
+  args: {
+    token: 'Abscsdkaoeiru(aoiçoai&raoroai??roa3"KcIEcUec',
+  } as TokenProps,
+}
