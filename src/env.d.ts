59a315f3a454f713617356344263c8c3008a49d8
Daniele Guido <gui.daniele@gmail.com>
2024-09-20 10:37:18 +0200

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,19 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly PUBLIC_VERSION: string
+  readonly PUBLIC_GIT_COMMIT_SHA: string
+  readonly PUBLIC_GIT_REMOTE: string
+  readonly PUBLIC_GIT_BRANCH: string
+  readonly PUBLIC_GIT_TAG: string
+  readonly PUBLIC_BUILD_DATE: string
+  readonly PUBLIC_IMPRESSO_DATALAB_BASE: string
+  readonly PUBLIC_IMPRESSO_DATALAB_SITE: string
+  readonly PUBLIC_IMPRESSO_API_HOST: string
+  readonly PUBLIC_IMPRESSO_API_PATH: string
+  readonly PUBLIC_IMPRESSO_WS_API_HOST: string
+  readonly PUBLIC_IMPRESSO_WS_API_PATH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
