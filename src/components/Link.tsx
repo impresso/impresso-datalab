7cf2eac079020866baf449f6b9df4b40b2cbec2c
Daniele Guido <gui.daniele@gmail.com>
2024-09-27 16:56:55 +0200

    add underline property to Link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,15 +2,24 @@ interface LinkProps extends React.HTMLProps<HTMLAnchorElement> {
   to: string
   children: React.ReactNode
   className?: string
+  underline?: boolean
 }
 
-const Link: React.FC<LinkProps> = ({ to = "/", children, className }) => {
+const Link: React.FC<LinkProps> = ({
+  to = "/",
+  children,
+  className = "",
+  underline = false,
+}) => {
   const href = [import.meta.env.PUBLIC_IMPRESSO_DATALAB_BASE, to]
     .join("/")
     .replace(/\/+/g, "/")
 
   return (
-    <a href={href} className={`text-decoration-none text-reset ${className}`}>
+    <a
+      href={href}
+      className={`${underline ? "text-decoration-underline" : "text-decoration-none"} text-reset ${className}`}
+    >
       {children}
     </a>
   )
