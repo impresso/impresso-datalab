06af513a73d9d1819a03d22f51246d47d4eda919
Daniele Guido <gui.daniele@gmail.com>
2024-09-27 16:39:11 +0200

    Update CodeSnippet.stories.tsx

--- src/stories/components/CodeSnippet.stories.tsx
+++ src/stories/components/CodeSnippet.stories.tsx
@@ -8,6 +8,13 @@ const CodeSnippetPropsValueSample = `
 text = "Mon nom est François et j'habite à Paris. (Reuter)"
 nlp(text)
 `
+const FromUpdatedNotebookCodeSnippetPropsValueSample = `
+\`\`\`python
+from impresso import AND
+
+impresso.search.find(title=AND("homme", "femme"))
+\`\`\`
+`
 
 const meta: Meta<typeof CodeSnippet> = {
   component: CodeSnippet,
@@ -25,5 +32,12 @@ export const Default: Story = {
   args: {
     value: CodeSnippetPropsValueSample.trim(),
     readonly: true,
+  } as CodeSnippetProps,
+}
+
+export const FromUpdatedNotebook: Story = {
+  args: {
+    value: FromUpdatedNotebookCodeSnippetPropsValueSample.trim(),
+    readonly: false,
   } as CodeSnippetProps,
 }
