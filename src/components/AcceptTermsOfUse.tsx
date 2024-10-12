87c8df24916dfc293d9f48129caf4f3f5dc6b272
Daniele Guido <gui.daniele@gmail.com>
2024-10-12 23:50:37 +0200

    Simplify component AcceptTermsOfuse

--- src/components/AcceptTermsOfUse.tsx
+++ src/components/AcceptTermsOfUse.tsx
@@ -1,25 +1,33 @@
-import { useState } from "react"
 import { Form } from "react-bootstrap"
+import type { FC, ChangeEvent } from "react"
 
-const AcceptTermsOfUse = () => {
-  const [accepted, setAccepted] = useState(false)
+interface AcceptTermsOfUseProps {
+  disabled?: boolean
+  checked?: boolean
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+}
 
-  const handleAccept = () => {
-    setAccepted((accepted) => !accepted)
-  }
-
+const AcceptTermsOfUse: FC<AcceptTermsOfUseProps> = ({
+  disabled = false,
+  checked = false,
+  onChange = () => {
+    console.log("AcceptTermsOfUse.onChange")
+  },
+}) => {
   return (
     <Form.Check>
       <Form.Check.Input
         type="checkbox"
         id="terms-of-use"
-        checked={accepted}
-        onChange={handleAccept}
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
       />
       <Form.Check.Label htmlFor="terms-of-use">
-        I accept the terms of use
+        I accept the terms of use {disabled ? "(disabled)" : ""}
       </Form.Check.Label>
     </Form.Check>
   )
 }
+
 export default AcceptTermsOfUse
