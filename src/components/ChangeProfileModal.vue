c9b0322a0ed45cd0e966f8aaf5bd6514a7ecc92e
Daniele Guido <gui.daniele@gmail.com>
2025-05-29 20:44:37 +0200

    add alert fr errors on changemodal

--- src/components/ChangeProfileModal.vue
+++ src/components/ChangeProfileModal.vue
@@ -4,6 +4,7 @@
     @close="() => emit('close')"
     title="Change Profile"
     hideFooter
+    dialogClass="SignUpModal modal-md-lg modal-dialog modal-dialog-centered modal-dialog-scrollable"
   >
     <template #modal-header="{ close }">
       <h4 class="modal-title h4">Change Profile</h4>
@@ -15,25 +16,44 @@
       ></button>
     </template>
     <ProfileForm
+      v-if="!isLoading"
       :mode="'edit'"
+      @submit="handleOnSubmit"
       :initialValues="signUpFormInitialValues"
       :show="show"
       bodyClass="px-3 pt-3 pb-0"
       dialogClass="SignUpModal modal-lg modal-dialog
     modal-dialog-centered modal-dialog-scrollable"
-    />
-    {{ signUpFormInitialValues }}
+    >
+      <template v-if="error" #form-errors>
+        <Alert class="mb-3" type="error">{{ error.message }}</Alert>
+      </template>
+      <template #submit-button="{ submit }">
+        <button
+          type="button"
+          @click="submit"
+          class="btn btn-primary btn-lg px-4"
+        >
+          <FloppyDiskArrowIn />
+          <span class="ms-2"> Save Changes</span>
+        </button>
+      </template>
+    </ProfileForm>
   </Modal>
 </template>
 <script lang="ts" setup>
 import Modal from "impresso-ui-components/components/legacy/BModal.vue"
-// import ChangePlanForm from "impresso-ui-components/components/ChangePlanForm.vue"
 import ProfileForm from "impresso-ui-components/components/ProfileForm.vue"
 import type { ProfileFormPayload } from "impresso-ui-components/components/ProfileForm.vue"
-// import { FloppyDiskArrowIn } from "@iconoir/vue"
-// import { AvailablePlansWithLabels } from "../constants"
 import { watch, ref } from "vue"
+import { FloppyDiskArrowIn } from "@iconoir/vue"
+import type { FeathersError } from "@feathersjs/errors"
+
 import { userService } from "../services"
+import Alert from "impresso-ui-components/components/Alert.vue"
+
+const isLoading = ref(true)
+const error = ref<FeathersError | null>(null)
 
 const signUpFormInitialValues = ref<ProfileFormPayload>({
   firstname: "",
@@ -43,21 +63,48 @@ const signUpFormInitialValues = ref<ProfileFormPayload>({
   email: "",
   password: "",
   repeatPassword: "",
+  pattern: "",
 })
 
 const fetchUser = async () => {
-  userService.find().then((data) => {
-    console.info("[ProfileModal] @useEffect - user", data)
-    signUpFormInitialValues.value = {
-      firstname: data.firstname,
-      lastname: data.lastname,
-      institutionalUrl: data.institutionalUrl,
-      affiliation: data.affiliation,
-      email: data.email,
-      password: "",
-      repeatPassword: "",
-    }
-  })
+  isLoading.value = true
+  userService
+    .find()
+    .then((data) => {
+      console.debug("[ProfileModal] fetchUser success:", data)
+      signUpFormInitialValues.value = {
+        firstname: data.firstname,
+        lastname: data.lastname,
+        institutionalUrl: data.institutionalUrl,
+        affiliation: data.affiliation,
+        email: data.email,
+        password: "",
+        repeatPassword: "",
+        pattern: data.pattern || "",
+      }
+    })
+    .catch((err: FeathersError) => {
+      error.value = err
+      console.warn(
+        "[RegisterModal] fetchUser error ",
+        err,
+        err.message,
+        err.data
+      )
+    })
+    .finally(() => {
+      isLoading.value = false
+    })
+}
+
+const handleOnSubmit = async (payload: ProfileFormPayload) => {
+  console.debug("[ChangeProfileModal] handleOnSubmit:", payload)
+  // try {
+  //   await userService.update(payload)
+  //   emit("success")
+  // } catch (error) {
+  //   console.error("[ChangeProfileModal] Error updating profile:", error)
+  // }
 }
 
 const props = withDefaults(
